b8ac8df6f4acf01f96a4613cd26f57c649ef860c
suyash1574 <zinjurke77h@gmail.com>
2025-03-10 22:47:36 +0530

    port added

--- app.py
+++ app.py
@@ -262,5 +262,6 @@ def clear():
     session.clear()  # Clear session data if needed
     return jsonify({"status": "success"})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Use the PORT environment variable or default to 5000
+    app.run(host="0.0.0.0", port=port)  # Bind to all interfaces
